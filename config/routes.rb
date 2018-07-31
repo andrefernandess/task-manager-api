3c14a27152299e6e056825d6395f4ec5dba41f21
andrefernandess <andre.l.fernandess@hotmail.com>
2018-07-31 17:22:37 +0000

    created session controller and action create

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
-  #devise_for :users
+  #sobrescreve os parametros do devise, para implementar apenas os methodos de session e serem aplicados no controller sessions criado e nao no controller padrao do devise
+  devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions' }
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :users, only: [:show, :create, :update, :destroy]
+      resources :sessions, only: [:create]
     end
   end
 end
