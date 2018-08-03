5ff1228f8866b5176b6a8b57860aba0f326240af
andrefernandess <andre.l.fernandess@hotmail.com>
2018-08-03 17:25:30 +0000

    adding update and destroy actions and tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -22,6 +22,23 @@ class Api::V1::TasksController < ApplicationController
         end
     end
 
+    def update
+        task = current_user.tasks.find(params[:id])
+
+        if task.update_attributes(tasks_params)
+            render json: task, status: 200
+        else
+            render json: { errors: task.errors }, status: 422
+        end
+    end
+
+    def destroy
+        task = current_user.tasks.find(params[:id])
+        task.destroy
+
+        head 204
+    end
+
     private
     def tasks_params
         params.require(:task).permit(:title, :description, :deadline, :done)
