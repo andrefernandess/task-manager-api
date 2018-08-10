4e680378e8736d71e15f5e148d9679530538f396
andrefernandess <andre.l.fernandess@hotmail.com>
2018-08-10 19:06:31 +0000

    inserted devise_auth_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,2 @@
-class ApplicationController < ActionController::API
-    include Authenticable
+class ApplicationController < ActionController::API  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require 'api_version_constraint'
 Rails.application.routes.draw do
   #sobrescreve os parametros do devise, para implementar apenas os methodos de session e serem aplicados no controller sessions criado e nao no controller padrao do devise
   devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions' }
+
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
     namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1) do
       resources :users, only: [:show, :create, :update, :destroy]
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
     end
 
     namespace :v2, path: '/', constraints: ApiVersionConstraint.new(version: 2, default: true) do
+      mount_devise_token_auth_for 'User', at: 'auth'
       resources :users, only: [:show, :create, :update, :destroy]
       resources :sessions, only: [:create, :destroy]
       resources :tasks, only: [:index, :show, :create, :update, :destroy]
